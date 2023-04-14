b5c0fbcb166c734b6ee3687498d5a5c8e6937c2f
Arlan Aliyev (Арлан Алиев) <104730663+HappyARL@users.noreply.github.com>
2023-04-14 19:05:22 +0600

    Update DijkstraVisitor.cpp

--- DijkstraVisitor.cpp
+++ DijkstraVisitor.cpp
@@ -6,6 +6,7 @@
 
 const int kMax = 2009000999;
 
+// структура ребра
 template <typename Vertex, typename Weight>
 struct DefaultEdge {
   Vertex src;
@@ -14,21 +15,26 @@ struct DefaultEdge {
   DefaultEdge(const Vertex& vertex_a, const Vertex& vertex_b,
               const Weight& value)
       : src(vertex_a), dst(vertex_b), weight(value) {}
+
   const Vertex& Source() const { return src; }
   const Vertex& Destination() const { return dst; }
   const Weight& WeightValue() const { return weight; }
 };
 
+// абстрактный граф
 template <typename Vertex, typename Weight,
           typename Edge = DefaultEdge<Vertex, Weight> >
 class AbstractGraph {
  public:
   using VertexType = Vertex;
   using EdgeType = Edge;
+
   explicit AbstractGraph(size_t vertices_num, size_t edges_num = 0)
       : vertices_number_(vertices_num), edges_number_(edges_num) {}
+
   size_t GetVerticesNumber() const { return vertices_number_; }
   size_t GetEdgesNumber() const { return edges_number_; }
+
   virtual std::vector<std::pair<Vertex, Weight> > GetNeighbours(
       const Vertex& vertex) = 0;
 
@@ -37,41 +43,83 @@ class AbstractGraph {
   size_t edges_number_ = 0;
 };
 
+// реализация на списке смежности
 template <typename Vertex, typename Weight,
           typename Edge = DefaultEdge<Vertex, Weight> >
 class Graph : public AbstractGraph<Vertex, Weight, Edge> {
  public:
   std::unordered_map<Vertex, std::vector<std::pair<Vertex, Weight> > >
-      connections;
+      list_neighbors;
+
   Graph(size_t vertices_num, const std::vector<Edge>& edges)
       : AbstractGraph<Vertex, Weight, Edge>(vertices_num, edges.size()) {
     for (const auto& edge : edges) {
-      connections[edge.Source()].push_back(
+      list_neighbors[edge.Source()].push_back(
           std::make_pair(edge.Destination(), edge.WeightValue()));
-      connections[edge.Destination()].push_back(
+      list_neighbors[edge.Destination()].push_back(
           std::make_pair(edge.Source(), edge.WeightValue()));
     }
   }
+
   std::vector<std::pair<Vertex, Weight> > GetNeighbours(
       const Vertex& vertex) final {
-    return connections[vertex];
+    return list_neighbors[vertex];
   }
 };
 
+// реализация на матрице смежности
+template <typename Vertex, typename Weight,
+          typename Edge = DefaultEdge<Vertex, Weight> >
+class GraphMatrix : public AbstractGraph<Vertex, Weight, Edge> {
+ public:
+  std::vector<std::vector<Weight> > matrix_neighbour;
+
+  GraphMatrix(size_t vertices_num, const std::vector<Edge>& edges)
+      : AbstractGraph<Vertex, Weight, Edge>(vertices_num, edges.size()) {
+    for (size_t i = 0; i < vertices_num; ++i) {
+      std::vector<Weight> tmp(vertices_num, 0);
+      matrix_neighbour.push_back(tmp);
+    }
+    for (const auto& edge : edges) {
+      matrix_neighbour[edge.Source()][edge.Destination()] = edge.WeightValue();
+      matrix_neighbour[edge.Destination()][edge.Source()] = edge.WeightValue();
+    }
+  }
+
+  std::vector<std::pair<Vertex, Weight> > GetNeighbours(
+      const Vertex& vertex) final {
+    std::vector<std::pair<Vertex, Weight> > result;
+    for (size_t i = 0; i < this->GetVerticesNumber(); ++i) {
+      if (matrix_neighbour[vertex][i] > 0) {
+        result.push_back(std::make_pair(i, matrix_neighbour[vertex][i]));
+      }
+    }
+    return result;
+  }
+  /*
+    void Print() {
+      for (size_t i = 0; i < this->GetVerticesNumber(); ++i) {
+        for (size_t j = 0; j < this->GetVerticesNumber(); ++j) {
+          std::cout << matrix_neighbour[i][j] << " ";
+        }
+        std::cout << '\n';
+      }
+    }
+  */
+};
+
+// Алгоритм Дейкстры
 template <typename Vertex, typename Weight,
           typename Edge = DefaultEdge<Vertex, Weight> >
 class DijkstraVisitor {
- private:
-  Graph<Vertex, Weight, Edge> graph_;
-  Vertex start_point_;
-
  public:
   DijkstraVisitor(Vertex start, Graph<Vertex, Weight, Edge> main_graph)
       : start_point_(start), graph_(main_graph) {}
+
   std::vector<Vertex> Dijkstra() {
     std::vector<Vertex> answer;
-    answer.resize(graph_.connections.size());
-    for (size_t i = 0; i < graph_.connections.size(); ++i) {
+    answer.resize(graph_.list_neighbors.size());
+    for (size_t i = 0; i < graph_.list_neighbors.size(); ++i) {
       answer[i] = kMax;
     }
     answer[start_point_] = 0;
@@ -80,21 +128,25 @@ class DijkstraVisitor {
     while (!distance.empty()) {
       int index = distance.begin()->second;
       distance.erase(distance.begin());
-      for (size_t j = 0; j < graph_.connections[index].size(); ++j) {
-        int sum = answer[index] + graph_.connections[index].at(j).second;
-        if (sum < answer[graph_.connections[index].at(j).first]) {
+      for (size_t j = 0; j < graph_.list_neighbors[index].size(); ++j) {
+        int sum = answer[index] + graph_.list_neighbors[index].at(j).second;
+        if (sum < answer[graph_.list_neighbors[index].at(j).first]) {
           distance.erase(
-              std::make_pair(answer[graph_.connections[index].at(j).first],
-                             graph_.connections[index].at(j).first));
-          answer[graph_.connections[index].at(j).first] = sum;
+              std::make_pair(answer[graph_.list_neighbors[index].at(j).first],
+                             graph_.list_neighbors[index].at(j).first));
+          answer[graph_.list_neighbors[index].at(j).first] = sum;
           distance.insert(
-              std::make_pair(answer[graph_.connections[index].at(j).first],
-                             graph_.connections[index].at(j).first));
+              std::make_pair(answer[graph_.list_neighbors[index].at(j).first],
+                             graph_.list_neighbors[index].at(j).first));
         }
       }
     }
     return answer;
   }
+
+ private:
+  Graph<Vertex, Weight, Edge> graph_;
+  Vertex start_point_;
 };
 
 int main() {
@@ -103,18 +155,20 @@ int main() {
   std::vector<std::vector<int> > total_answer;
   while (maps != 0) {
     size_t rooms;
-    int connections;
-    std::cin >> rooms >> connections;
+    int list_neighbors;
+    std::cin >> rooms >> list_neighbors;
     std::vector<DefaultEdge<int, int> > edges;
-    while (connections != 0) {
+    while (list_neighbors != 0) {
       int room_out;
       int room_in;
       int weight;
       std::cin >> room_out >> room_in >> weight;
       edges.push_back(DefaultEdge<int, int>(room_out, room_in, weight));
-      --connections;
+      --list_neighbors;
     }
     Graph<int, int, DefaultEdge<int, int> > graph(rooms, edges);
+    // GraphMatrix<int, int, DefaultEdge<int, int> > graph_matrix(rooms, edges);
+    // graph_matrix.Print(); - для проверки работоспособности
     int start_point;
     std::cin >> start_point;
     std::cout << std::endl;
